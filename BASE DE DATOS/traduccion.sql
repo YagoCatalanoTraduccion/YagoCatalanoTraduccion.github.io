762ece45cbc95b7d007d5a1a891cc2e29b2cd4de
YagoCatalanoTraduccion <160546133+YagoCatalanoTraduccion@users.noreply.github.com>
2024-02-23 21:14:47 +0100

    Add files via upload

--- BASE DE DATOS/traduccion.sql
+++ BASE DE DATOS/traduccion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-02-2024 a las 10:10:30
+-- Tiempo de generación: 21-02-2024 a las 10:08:04
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `contactos`
+--
+
+CREATE TABLE `contactos` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `ape` varchar(30) NOT NULL,
+  `correo` varchar(30) NOT NULL,
+  `coms` varchar(30) NOT NULL,
+  `archivo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `solicitud`
 --
 
@@ -38,6 +53,15 @@ CREATE TABLE `solicitud` (
   `archivo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `solicitud`
+--
+
+INSERT INTO `solicitud` (`id`, `nom`, `ape`, `correo`, `coms`, `idio`, `idid`, `archivo`) VALUES
+(2, 'Yago', 'Catalano Andujar', 'catalanoyagocolegio15@gmail.comaswde', 'asdasd', 'espanol', 'frances', 'TFG.pdf'),
+(3, 'Andrés', 'Marin Calcero', 'amarinc04@educarex.es', 'No me gustan las faltas ortograficas', 'ingles', 'espanol', 'Cita_Medico.pdf'),
+(4, 'Oscar', 'Arroyo', 'oarroyod02@gmail.com', 'asdasd', 'aleman', 'catalan', 'Documento sin título.pdf');
+
 -- --------------------------------------------------------
 
 --
@@ -48,12 +72,28 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nom_usu` varchar(30) NOT NULL,
   `contra` varchar(30) NOT NULL,
+  `correo` varchar(30) NOT NULL,
   `tipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nom_usu`, `contra`, `correo`, `tipo`) VALUES
+(1, 'Hoal', 'asd', 'hoalsd@gmail.com', 1),
+(2, 'Asd', '6789', 'asd1234@gmail.com', 1),
+(3, 'Yago', '1234', 'ycatalanoa01@educarex.es', 2);
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `contactos`
+--
+ALTER TABLE `contactos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `solicitud`
@@ -72,16 +112,22 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `contactos`
+--
+ALTER TABLE `contactos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `solicitud`
 --
 ALTER TABLE `solicitud`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
